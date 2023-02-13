038796cb0f62ea2ee4b161e283c78366ddc0b741
Aniket Rane <aniketrane0557@gmail.com>
2023-02-13 12:43:46 +0530

    Application 1st stage

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     global: "globalThis",
     "process.env": {},
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
 });
